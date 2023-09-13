e5f410162c3f06ee774e59ba3755ae8e8356fba1
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-09-13 15:56:19 +0300

    Submiting to read the errors on "readsDataCorrectly"

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,24 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Paths;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = "";
+        int age = 0;
+        String email = "";
+        long phone = 0L;
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            name = reader.readLine();
+            age = Integer.parseInt(reader.readLine());
+            email = reader.readLine();
+            phone = Long.parseLong(reader.readLine());
+        } catch (IOException e) {
+
+        }
+        return new Profile(name, age, email, phone);
     }
 }
