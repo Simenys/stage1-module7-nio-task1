9260cf9444480d3700ae7c099880328d629042af
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-09-13 20:15:08 +0300

    Task solved

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,24 +1,39 @@
 package com.epam.mjc.nio;
 
 import java.io.*;
-import java.nio.file.Paths;
+
 
 
 public class FileReader {
 
+
     public Profile getDataFromFile(File file) {
         String name = "";
-        int age = 0;
+        String age = "";
         String email = "";
-        long phone = 0L;
+        String phone = "";
         try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
             name = reader.readLine();
-            age = Integer.parseInt(reader.readLine());
+            age = reader.readLine();
             email = reader.readLine();
-            phone = Long.parseLong(reader.readLine());
+            phone = reader.readLine();
         } catch (IOException e) {
 
         }
-        return new Profile(name, age, email, phone);
+
+
+        String nameTrim = name.substring(name.indexOf(":") + 2);
+        nameTrim.trim();
+
+        String ageTrim = age.substring(age.indexOf(":") + 2);
+        ageTrim.trim();
+
+        String emailTrim = email.substring(email.indexOf(":") + 2);
+        emailTrim.trim();
+
+        String phoneTrim = phone.substring(phone.indexOf(":") + 2);
+        phoneTrim.trim();
+
+        return new Profile(nameTrim, Integer.parseInt(ageTrim), emailTrim, Long.parseLong(phoneTrim));
     }
 }
